c6a03b39c4b15e28e6b51eaaed3c03c1935acde4
siamtx <siamtx@yahoo.com>
2025-01-14 21:02:58 -0500

    Finished the 3 core requirements.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
@@ -8,28 +9,65 @@ class Program
         string grade = Console.ReadLine();
         int number = int.Parse(grade);
 
+        /***********************************************
         if (number >= 90)
         {
-            Console.Write("A");
+            Console.WriteLine("A");
         }
         else if (number >= 80 && number < 90)
         {
-            Console.Write("B");
+            Console.WriteLine("B");
         }
         else if (number >= 70 && number < 80)
         {
-            Console.Write("C");
+            Console.WriteLine("C");
         }
         else if (number >= 60 && number < 70)
         {
-            Console.Write("D");
+            Console.WriteLine("D");
         }
         else if (number < 60)
         {
-            Console.Write("F");
+            Console.WriteLine("F");
         }
 
-        if (!(D ||F))
+        if (number >= 70)
+        {
+            Console.Write("Congratulations! You passed the course");
+        }
+        else
+        {
+            Console.Write("Keep trying! You will get it next time.");
+        }
+
+        **/
+
+        string letter ="";
+
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 80 && number < 90)
+        {
+            letter = "B";
+        }
+        else if (number >= 70 && number < 80)
+        {
+            letter = "C";
+        }
+        else if (number >= 60 && number < 70)
+        {
+            letter = "D";
+        }
+        else if (number < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if (number >= 70)
         {
             Console.Write("Congratulations! You passed the course");
         }
