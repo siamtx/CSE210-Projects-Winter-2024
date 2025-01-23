dc62512412fcbcb154d417fa2f83b056ec8ed66b
siamtx <siamtx@yahoo.com>
2025-01-22 23:07:37 -0500

    completed Lesson02.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,22 +5,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
-
         Job job1 = new Job();
-
         job1._company = "Hildago Codes";
         job1._jobTitle = "Software Engineer";
-
-        Console.WriteLine(job1._company);
-        Console.WriteLine(job1._jobTitle);
+        job1._startYear = 2012;
+        job1._endYear = 2016;
 
         Job job2 = new Job();
-
         job2._company = "Coding is Fun";
-        job2._jobTitle = "Software Engineer Manager";
+        job2._jobTitle = "Senior Software Engineer";
+        job2._startYear = 2016;
+        job2._endYear = 2020;
 
-        Console.WriteLine(job2._company);
-        Console.WriteLine(job2._jobTitle);
+        // Call to print function
+        // job1.DisplayJobDetails();
+        // job2.DisplayJobDetails();
+
+        Resume myResume = new Resume();
+        myResume._name = "Ricky Bobby";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
