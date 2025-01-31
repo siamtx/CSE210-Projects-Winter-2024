6c5777ced1f446ad32b3c1d58ba454714cf1c655
siamtx <siamtx@yahoo.com>
2025-01-30 23:26:41 -0500

    added a menu text to the program.cs file.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,11 +1,29 @@
 using System;
+using System.ComponentModel.Design;
+using System.Net;
+using System.Xml.Serialization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+
+
+    MenuCommand:
+        "1. Write a new entry.""
+        "2. Display the Journal"
+        "3. Save the Journal to a file"
+        "4. Load the Journal from a file"
+        "5. Quit"
     }
+
+    // Do while loop to execute the program
+    
+    do
+    {
+        Console.Writeline()
+} while () ;
 
 
 }
