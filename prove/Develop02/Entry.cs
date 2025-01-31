0379939f4f625ed34101a2d727f4c87bebe0909a
siamtx <siamtx@yahoo.com>
2025-01-30 21:58:47 -0500

    upated classes in Develop02 for team meeting.

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -2,5 +2,12 @@ using System;
 
 class Entry
 {
-    
+    string _userinput = " ";
+    string _dateTime = " ";
+
+    public void Display()
+    {
+        Console.WriteLine("_dateTime");
+    }
+
 }
--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,6 +1,20 @@
 using System;
+using Microsoft.VisualBasic;
 
 class Journal
 {
-    
+    List<string> book = new List<string>();
+
+    book.Add("poo");
+
+    book.Save(book);
+
+    book.Load(book);
+
+    public void Display()
+    {
+        Console.WriteLine();
+    }
+
+
 }
